093e0491a02cd2ba6cd8b733dfb9799e5c9842a3
eduardatuboy <eduardatuboy@gmail.com>
2026-06-10 04:39:05 -0300

    chore(src/assets): new and improved images for hair of customizable avatar

--- frontend/src/components/villager.jsx
+++ frontend/src/components/villager.jsx
@@ -47,6 +47,40 @@ import hair5 from "../assets/custom_villager/hair5.png";
 import hair6 from "../assets/custom_villager/hair6.png";
 import hair7 from "../assets/custom_villager/hair7.png";
 import hair8 from "../assets/custom_villager/hair8.png";
+import hair9 from "../assets/custom_villager/hair9.png";
+import hair10 from "../assets/custom_villager/hair10.png";
+import hair11 from "../assets/custom_villager/hair11.png";
+import hair12 from "../assets/custom_villager/hair12.png";
+import hair13 from "../assets/custom_villager/hair13.png";
+import hair14 from "../assets/custom_villager/hair14.png";
+import hair15 from "../assets/custom_villager/hair15.png";
+import hair16 from "../assets/custom_villager/hair16.png";
+import hair17 from "../assets/custom_villager/hair17.png";
+import hair18 from "../assets/custom_villager/hair18.png";
+import hair19 from "../assets/custom_villager/hair19.png";
+import hair20 from "../assets/custom_villager/hair20.png";
+import hair21 from "../assets/custom_villager/hair21.png";
+import hair22 from "../assets/custom_villager/hair22.png";
+import hair23 from "../assets/custom_villager/hair23.png";
+import hair24 from "../assets/custom_villager/hair24.png";
+import hair25 from "../assets/custom_villager/hair25.png";
+import hair26 from "../assets/custom_villager/hair26.png";
+import hair27 from "../assets/custom_villager/hair27.png";
+import hair28 from "../assets/custom_villager/hair28.png";
+import hair29 from "../assets/custom_villager/hair29.png";
+import hair30 from "../assets/custom_villager/hair30.png";
+import hair31 from "../assets/custom_villager/hair31.png";
+import hair32 from "../assets/custom_villager/hair32.png";
+import hair33 from "../assets/custom_villager/hair33.png";
+import hair34 from "../assets/custom_villager/hair34.png";
+import hair35 from "../assets/custom_villager/hair35.png";
+import hair36 from "../assets/custom_villager/hair36.png";
+import hair37 from "../assets/custom_villager/hair37.png";
+import hair38 from "../assets/custom_villager/hair38.png";
+import hair39 from "../assets/custom_villager/hair39.png";
+import hair40 from "../assets/custom_villager/hair40.png";
+import hair41 from "../assets/custom_villager/hair41.png";
+import hair42 from "../assets/custom_villager/hair42.png";
 
 const skins = [
   skin1,
@@ -90,7 +124,50 @@ const eyes = [
   eyes25,
   eyes26,
 ];
-const hairs = [hair1, hair2, hair3, hair4, hair5, hair6, hair7, hair8];
+const hairs = [
+  hair1,
+  hair2,
+  hair3,
+  hair4,
+  hair5,
+  hair6,
+  hair7,
+  hair8,
+  hair9,
+  hair10,
+  hair11,
+  hair12,
+  hair13,
+  hair14,
+  hair15,
+  hair16,
+  hair17,
+  hair18,
+  hair19,
+  hair20,
+  hair21,
+  hair22,
+  hair23,
+  hair24,
+  hair25,
+  hair26,
+  hair27,
+  hair28,
+  hair29,
+  hair30,
+  hair31,
+  hair32,
+  hair33,
+  hair34,
+  hair35,
+  hair36,
+  hair37,
+  hair38,
+  hair39,
+  hair40,
+  hair41,
+  hair42,
+];
 
 function AttributeControl({ label, currentIndex, totalItems, setIndex }) {
   const nextItem = (current, total) => (current + 1) % total;
