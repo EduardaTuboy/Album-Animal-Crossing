f1e480bebbc187b66f90b87f52ad7b64b8649872
eduardatuboy <eduardatuboy@gmail.com>
2026-06-10 02:39:20 -0300

    chore(src/assets): new and improved images for eyes and skin of customizable avatar

--- frontend/src/components/villager.jsx
+++ frontend/src/components/villager.jsx
@@ -9,6 +9,10 @@ import skin5 from "../assets/custom_villager/skin5.png";
 import skin6 from "../assets/custom_villager/skin6.png";
 import skin7 from "../assets/custom_villager/skin7.png";
 import skin8 from "../assets/custom_villager/skin8.png";
+import skin9 from "../assets/custom_villager/skin9.png";
+import skin10 from "../assets/custom_villager/skin10.png";
+import skin11 from "../assets/custom_villager/skin11.png";
+import skin12 from "../assets/custom_villager/skin12.png";
 import eyes1 from "../assets/custom_villager/eyes1.png";
 import eyes2 from "../assets/custom_villager/eyes2.png";
 import eyes3 from "../assets/custom_villager/eyes3.png";
@@ -17,6 +21,24 @@ import eyes5 from "../assets/custom_villager/eyes5.png";
 import eyes6 from "../assets/custom_villager/eyes6.png";
 import eyes7 from "../assets/custom_villager/eyes7.png";
 import eyes8 from "../assets/custom_villager/eyes8.png";
+import eyes9 from "../assets/custom_villager/eyes9.png";
+import eyes10 from "../assets/custom_villager/eyes10.png";
+import eyes11 from "../assets/custom_villager/eyes11.png";
+import eyes12 from "../assets/custom_villager/eyes12.png";
+import eyes13 from "../assets/custom_villager/eyes13.png";
+import eyes14 from "../assets/custom_villager/eyes14.png";
+import eyes15 from "../assets/custom_villager/eyes15.png";
+import eyes16 from "../assets/custom_villager/eyes16.png";
+import eyes17 from "../assets/custom_villager/eyes17.png";
+import eyes18 from "../assets/custom_villager/eyes18.png";
+import eyes19 from "../assets/custom_villager/eyes19.png";
+import eyes20 from "../assets/custom_villager/eyes20.png";
+import eyes21 from "../assets/custom_villager/eyes21.png";
+import eyes22 from "../assets/custom_villager/eyes22.png";
+import eyes23 from "../assets/custom_villager/eyes23.png";
+import eyes24 from "../assets/custom_villager/eyes24.png";
+import eyes25 from "../assets/custom_villager/eyes25.png";
+import eyes26 from "../assets/custom_villager/eyes26.png";
 import hair1 from "../assets/custom_villager/hair1.png";
 import hair2 from "../assets/custom_villager/hair2.png";
 import hair3 from "../assets/custom_villager/hair3.png";
@@ -26,8 +48,48 @@ import hair6 from "../assets/custom_villager/hair6.png";
 import hair7 from "../assets/custom_villager/hair7.png";
 import hair8 from "../assets/custom_villager/hair8.png";
 
-const skins = [skin1, skin2, skin3, skin4, skin5, skin6, skin7, skin8];
-const eyes = [eyes1, eyes2, eyes3, eyes4, eyes5, eyes6, eyes7, eyes8];
+const skins = [
+  skin1,
+  skin2,
+  skin3,
+  skin4,
+  skin5,
+  skin6,
+  skin7,
+  skin8,
+  skin9,
+  skin10,
+  skin11,
+  skin12,
+];
+const eyes = [
+  eyes1,
+  eyes2,
+  eyes3,
+  eyes4,
+  eyes5,
+  eyes6,
+  eyes7,
+  eyes8,
+  eyes9,
+  eyes10,
+  eyes11,
+  eyes12,
+  eyes13,
+  eyes14,
+  eyes15,
+  eyes16,
+  eyes17,
+  eyes18,
+  eyes19,
+  eyes20,
+  eyes21,
+  eyes22,
+  eyes23,
+  eyes24,
+  eyes25,
+  eyes26,
+];
 const hairs = [hair1, hair2, hair3, hair4, hair5, hair6, hair7, hair8];
 
 function AttributeControl({ label, currentIndex, totalItems, setIndex }) {
